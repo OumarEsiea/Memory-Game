e95862c597e710374c86f7a63fe99fd21b920fbb
Sy <ousy@et.esiea.fr>
2023-05-27 12:02:22 +0200

    Changes

--- Memo.js
+++ Memo.js
@@ -1,13 +1,16 @@
 class MemoryGame {
-    constructor(size=6) {
+    constructor(size) {
         this.size = size;
         this.images = [];
-       
+        this.time=document.getElementById(`timer2`).value
         this.selectedImages = [];
+        this.count ;
+        this.Premierchoix = null;
+        this.Secondchoix = null;
     }
   
     Init() {
-        this.images = ["0.png","1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png","12.png"];
+        this.images = ["1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png","12.png"];
         // remplisser le tableau avec les id de vos images 
     }
   
@@ -16,7 +19,7 @@ class MemoryGame {
         const list = document.getElementById("list");
         let htmltags = "";
         for(let i = 0; i<((this.size)*2); i++) {
-            htmltags = htmltags + '<li class="flex-item"><a href="#"><img id="item' + i + '" width="50%" onclick=showCard('+i+') src="20.png"></a></li>';
+            htmltags = htmltags + '<li class="flex-item"><a href="#"><img id="item' + i + '" width="50%" onclick=memoryGameTest.showCard('+i+') src="20.png"></a></li>';
         }
         list.innerHTML = htmltags;
   
@@ -34,7 +37,7 @@ class MemoryGame {
         for(let i=0;i<((this.size)*2);i++) {
             document.getElementById(`item${i}`).src = this.images[this.selectedImages[i]];
         }
-        console.log("Deck Shuffled!");
+        console.log("Deck Shuffled!")
     }
   
     Play() {
@@ -53,17 +56,28 @@ class MemoryGame {
       const list = document.getElementById("list");
       let htmltags = "";
       for(let i = 0; i<this.size*2; i++) {
-          htmltags = htmltags + '<li class="flex-item"><a href="#"><img id="item' + i + '" width="50%" onclick=showCard('+i+') src="21.png"></a></li>';
+          htmltags = htmltags + '<li class="flex-item"><a href="#"><img id="item' + i + '" width="50%" src="21.png"></a></li>';
       }
       list.innerHTML = htmltags;
+    }
+
+    timer(){
+        
+        var minuteur = document.getElementById(`Timer`)
+        minuteur.innerText = `Timer : ${this.time}` 
+        this.time --;
+
+
+        if (this.time == -2){
+        window.alert(`Le temps de jeu est écoulé`)
+        location.reload()
+        }
     }
     showCard(id){
         var image = document.getElementById(`item${id}`)    
         image.src = this.images[this.selectedImages[id]];
-    
-      }
   }  
-  
+}
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -72,21 +86,20 @@ class MemoryGame {
 
   function startGame() {
     let difficulty = document.getElementById(`size`).value;
-    var memoryGameTest = new MemoryGame(difficulty);
+    memoryGameTest = new MemoryGame(difficulty);
     memoryGameTest.Play();
     let tmp = document.getElementById(`timer`).value;
     setTimeout(function() {memoryGameTest.Hidden();}, tmp*1000);
-    console.log(difficulty)
+    var horloge = setInterval(function() {memoryGameTest.timer();}, 1000);
   }
 
   function HiddenGame() {
-    // var difficulty = document.getElementById(`size`).value;
     var memoryGameTest = new MemoryGame();
     memoryGameTest.Hidden();
   }
   
   function reloadGame() {
     location.reload()
-    var memoryGameTest = new MemoryGame();
-    memoryGameTest.reload();
   }
+
+  // Ajouter ou non une méthode a un objet classList.remove("Nom de la méthode ou de la classe")
